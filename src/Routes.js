434b03a078d4a6815548212228183b0b556edb73
csergey8 <c.sergey@protonmail.com>
2018-11-13 11:43:22 +0200

    matches component with table finished

--- src/Routes.js
+++ src/Routes.js
@@ -7,12 +7,14 @@ import SignIn from './components/signin/index';
 import Dashboard from './components/admin/Dashboard';
 import PrivateRoutes from './components/authRoutes/privateRoutes';
 import PublicRoutes from './components/authRoutes/publicRoutes';
+import AdminMatches from './components/admin/matches/index';
 
 
 const Routes = (props) => {
   return (
     <Layout>
       <Switch>
+      <PrivateRoutes {...props} path="/admin_matches" exact component={AdminMatches} />
         <PrivateRoutes {...props} path="/dashboard" exact component={Dashboard} />
         <PublicRoutes {...props} restricted={true} path="/sign_in" exact component={SignIn} />
         <PublicRoutes {...props} restricted={false} path="/" exact component={Home} />
