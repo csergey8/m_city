3c17514847b989d3e3835ac8551de926f8c7630f
csergey8 <c.sergey@protonmail.com>
2018-11-14 13:20:37 +0200

    list players component finished

--- src/Routes.js
+++ src/Routes.js
@@ -9,6 +9,7 @@ import PrivateRoutes from './components/authRoutes/privateRoutes';
 import PublicRoutes from './components/authRoutes/publicRoutes';
 import AdminMatches from './components/admin/matches/index';
 import AddEditMatch from './components/admin/matches/addEditMatch';
+import AdminPlayers from './components/admin/players/index';
 
 
 const Routes = (props) => {
@@ -18,6 +19,7 @@ const Routes = (props) => {
         <PrivateRoutes {...props} path="/admin_matches/edit_match/:id" exact component={AddEditMatch} />
         <PrivateRoutes {...props} path="/admin_matches/edit_match" exact component={AddEditMatch} />
         <PrivateRoutes {...props} path="/admin_matches" exact component={AdminMatches} />
+        <PrivateRoutes {...props} path="/admin_players" exact component={AdminPlayers} />
         <PrivateRoutes {...props} path="/dashboard" exact component={Dashboard} />
         <PublicRoutes {...props} restricted={true} path="/sign_in" exact component={SignIn} />
         <PublicRoutes {...props} restricted={false} path="/" exact component={Home} />
---
+++ src/components/admin/players/index.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import AdminLayout from '../../../hoc/AdminLayout';
+import { Link } from 'react-router-dom';
+import { Table, TableBody, TableCell, TableHead, TableRow, Paper, CircularProgress } from '@material-ui/core';
+
+import { firebaseMatches, firebasePlayers } from '../../../firebase';
+import { firebaseLooper, reverseArray } from '../../ui/misc'; 
+
+export default class AdminPlayers extends Component {
+  state = {
+    isLoading: true,
+    players: []
+  }
+
+  componentDidMount() {
+    firebasePlayers.once('value')
+      .then(snap => {
+        const players = firebaseLooper(snap);
+
+        this.setState({
+          isLoading: false,
+          players: reverseArray(players)
+        })
+      })
+      .catch(err => console.log(err))
+
+  } 
+
+  render() {
+    console.log(this.state)
+    return (
+      <AdminLayout>
+        <div>
+          <Paper>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>First name</TableCell>
+                  <TableCell>Last name</TableCell>
+                  <TableCell>Number</TableCell>
+                  <TableCell>Position</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {this.state.players ?
+                    this.state.players.map((player, i) => (
+                      <TableRow key={i}>
+                        <TableCell>
+                          <Link to={`/admin_players/add_players/${player.id}`}>
+                            {player.name}
+                          </Link>
+                        </TableCell>
+                        <TableCell>
+                          <Link to={`/admin_players/add_players/${player.id}`}>
+                            {player.lastname}
+                          </Link>
+                        </TableCell>
+                        <TableCell>
+                            {player.number}
+                        </TableCell>
+                        <TableCell>
+                            {player.position}
+                        </TableCell>
+                      </TableRow>
+                    ))
+                :null
+                }
+              </TableBody>
+            </Table>
+          </Paper>
+        
+          <div className="admin_progress">
+            {
+              this.state.isLoading ?
+              <CircularProgress thickness={7} style={{ color: '#98c5e9'}} />
+              :
+              null
+            }
+          </div>
+        </div>
+      </AdminLayout>
+      
+    )
+  }
+}
